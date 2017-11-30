c54b86f5b22e002d2943b6e38954b415797e9ff3
KrangPi <jzi@inbox.ru>
2017-11-30 16:04:35 +0300

    work ver. of loop in serial_write.py, add TempSensorSPI.py

--- serial_read.py
+++ serial_read.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
-      
-  
+#-*- coding: utf-8 -*-
+
 import time
 import serial
 
 
 ser = serial.Serial(
-  
-   port='/dev/ttyUSB0',
-   baudrate = 9600,
-   parity=serial.PARITY_NONE,
-   stopbits=serial.STOPBITS_ONE,
-   bytesize=serial.EIGHTBITS,
-   timeout=1
-)
-counter=0
 
+    port='/dev/ttyUSB0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+if  ser.is_open == True:
+    ser.close()
 
 while 1:
-   x=ser.readline()
-   print ( x )
+    print( ser.name )
+    ser.open()
+    ser.write(b'%s\n'%(ser.name.encode('ascii')))
+    x=ser.readline()
+    print ( x )
+    ser.close()
--- serial_write.py
+++ serial_write.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-      
-      
+# -*- coding: utf-8 -*-
+
 import time
 import serial
 
 
 ser = serial.Serial(
-  
-   port='/dev/ttyAMA0',
-   baudrate = 9600,
-   parity=serial.PARITY_NONE,
-   stopbits=serial.STOPBITS_ONE,
-   bytesize=serial.EIGHTBITS,
-   timeout=1
+
+    port='/dev/ttyS0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
 )
 counter=0
-
-
-while 1:
-   ser.write(('Write counter: %d \n'%(counter)).encode('ascii'))
-   time.sleep(1)
-   counter += 1
+if  ser.is_open ==  True:
+    ser.close()
+while True:
+    ser.open()
+    #ser.write( b'Write counter: %d \n'%(counter) )
+    str = 'write to ' + ser.name + ' counter: %d'%(counter)
+    print ( str )
+    #ser.write( str.encode('ascii') )
+    ser.write(b'%s %d\n'%(str.encode('ascii'), counter) )
+    print ( 'read from %s counter: %s\n'%(ser.name, ser.readline()) )
+    ser.close()
+    time.sleep(1)
+    counter += 1
